0eb7dace7475a3da9b73b6ce6c23c8f64c2ac9ea
Sean Amadio <sean.amadio@gmail.com>
2014-10-21 01:15:49 -0400

    Initial Commit

---
+++ CanvasBlittingTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CanvasBlittingTool
+//
+//  Created by Sean Amadio on 2014-10-21.
+//  Copyright (c) 2014 Two Amadios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CanvasBlittingTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CanvasBlittingTool
+//
+//  Created by Sean Amadio on 2014-10-21.
+//  Copyright (c) 2014 Two Amadios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
