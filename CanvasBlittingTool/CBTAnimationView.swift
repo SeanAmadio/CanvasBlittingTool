e75a3dcf9341fca562df01be12443f12e60a3758
Sean Amadio <sean.amadio@gmail.com>
2014-10-26 15:06:54 -0400

    Added a subclass of image view to handle the display link and animations

---
+++ CanvasBlittingTool/CBTAnimationView.swift
@@ -0,0 +1,19 @@
+//
+//  CBTAnimationView.swift
+//  CanvasBlittingTool
+//
+//  Created by Sean Amadio on 2014-10-26.
+//  Copyright (c) 2014 Two Amadios. All rights reserved.
+//
+
+import Cocoa
+
+class CBTAnimationView: NSImageView {
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
