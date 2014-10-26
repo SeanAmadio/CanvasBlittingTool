ecba3b8dc11d60f84b483063bdeaef8b29724449
Sean Amadio <sean.amadio@gmail.com>
2014-10-26 18:26:15 -0400

    Spent some time wrestling with Swift and CoreVideo to get the display link callback working

--- CanvasBlittingTool/CBTAnimationView.swift
+++ CanvasBlittingTool/CBTAnimationView.swift
@@ -10,6 +10,34 @@ import Cocoa
 
 class CBTAnimationView: NSImageView {
 
+    var displayLink: Unmanaged<CVDisplayLink>?;
+    
+    override init() {
+        super.init();
+        CVDisplayLinkCreateWithActiveCGDisplays(&displayLink);
+        
+        
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder);
+    }
+    
+    func dlCallback(displayLink: CVDisplayLink!,
+        inNow: UnsafePointer<CVTimeStamp>,
+        inOutputTime: UnsafePointer<CVTimeStamp>,
+        flagsIn: CVOptionFlags,
+        flagsOut: UnsafeMutablePointer<CVOptionFlags>,
+        context: UnsafeMutablePointer<Void>) -> CVReturn {
+            let viewPoint = UnsafeMutablePointer<CBTAnimationView>(context);
+            return viewPoint.memory.drawFrame(inOutputTime);
+    }
+    
+    func drawFrame (outputTime: UnsafePointer<CVTimeStamp>) -> CVReturn
+    {
+        return 0;
+    }
+
     override func drawRect(dirtyRect: NSRect) {
         super.drawRect(dirtyRect)
 
