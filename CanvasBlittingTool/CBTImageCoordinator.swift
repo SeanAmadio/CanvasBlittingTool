85e8112e01bf798615b2ac26291c7d2d16043f30
Sean Amadio <sean.amadio@gmail.com>
2015-03-03 13:38:58 -0500

    Implemented most of the Image Coordinator functionality

--- CanvasBlittingTool/CBTImageCoordinator.swift
+++ CanvasBlittingTool/CBTImageCoordinator.swift
@@ -10,15 +10,32 @@ import Cocoa
 
 class CBTImageCoordinator: CBTCoordinator
 {
-    var image:NSImage!;
-    var currentDrawPoint = CGPointZero;
+    var image:NSImage;
+    var width:Float = 1024.0;
+    var position = BlockPoint(x: 0, y: 0);
     
-    func addBlock(block: NSImage, callbackClosure: (CGPoint) -> Void)
+    override init()
+    {
+        image = NSImage(size: CGSizeMake(CGFloat(self.width), CGFloat(self.width)));
+        super.init();
+    }
+    
+    func addBlock(block: NSImage, callbackClosure: (BlockPoint) -> Void)
     {
         var operation = NSBlockOperation { () -> Void in
             //Write block at current draw point
+            self.image.lockFocus();
+            block.drawAtPoint(self.position.pixelPoint.point, fromRect: CGRectZero, operation: NSCompositingOperation.CompositeCopy, fraction: 1.0);
+            self.image.unlockFocus();
             //Callback with point where image was drawn
+            callbackClosure(self.position);
             //Move draw point forward
+            self.position.x++;
+            if (self.position.pixelPoint.x == Int(self.width))
+            {
+                self.position.x = 0;
+                self.position.y++;
+            }
         };
         self.modifyQueue.addOperation(operation);
     }
