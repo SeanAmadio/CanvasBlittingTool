aad289084155c40f86bacd8584e4fd936dcac242
Sean Amadio <sean.amadio@gmail.com>
2015-03-06 17:37:01 -0500

    Successfully implemented the needsWrite function. Only tolerance 0.0 has been tested so far

--- CanvasBlittingTool/CIImage+Operations.swift
+++ CanvasBlittingTool/CIImage+Operations.swift
@@ -11,7 +11,30 @@ import AppKit
 
 extension CIImage
 {
+    //Accepts a tolerance from 0-1.0 when testing for a match, 0 meaning the blocks must be exact and 1 meaning they can be completely different
+    func needsWrite(tolerance: Float) -> Bool
+    {
+        //Get the maximum pixel value in the difference image
+        let extent = CIVector(x: self.extent().origin.x, y: self.extent().origin.y, z: self.extent().size.width, w: self.extent().size.height);
+        let maximum = CIFilter(name:"CIAreaMaximum");
+        maximum.setValue(self, forKey:kCIInputImageKey);
+        maximum.setValue(extent, forKey:kCIInputExtentKey);
+        
+        //Get the raw colour data from the single pixel maximum
+        let bytesPerPixel = 4;
+        var rawData:[CUnsignedChar] = [0,0,0,0];
+        let context:CIContext? = NSGraphicsContext(bitmapImageRep: NSBitmapImageRep(CIImage: maximum.outputImage))!.CIContext;
+        context!.render(maximum.outputImage, toBitmap: &rawData, rowBytes: 4, bounds: maximum.outputImage.extent(), format: kCIFormatARGB8, colorSpace: CGColorSpaceCreateWithName(kCGColorSpaceGenericRGBLinear));
+        
+        //Block Difference is the difference between blocks of two frames from 0-765
+        let blockDifference = Int(rawData[1])+Int(rawData[2])+Int(rawData[3]);
+        let acceptedDifference = Int(765*tolerance);
+        
+        //If the block difference is within the accepted difference return false (no write needed), otherwise a write is needed
+        return !(blockDifference <= acceptedDifference);
+    }
     
+    //Writes the CIImage to a PNG in the documents folder with the given name
     func writeToPNG(name: String) -> Bool
     {
         var docsDir:AnyObject = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0];
@@ -21,6 +44,7 @@ extension CIImage
         return data!.writeToFile(databasePath, atomically: false);
     }
     
+    //Returns a new cropped image at 0, 0
     func imageByCroppingToRectWithTranslate(rect: CGRect) -> CIImage
     {
         var croppedImage = self.imageByCroppingToRect(rect);
@@ -29,8 +53,8 @@ extension CIImage
         let affineTransform = NSAffineTransform();
         
         affineTransform.translateXBy(-rect.origin.x, yBy:-rect.origin.y);
-        transform.setValue(affineTransform, forKey:"inputTransform");
-        transform.setValue(croppedImage, forKey:"inputImage");
+        transform.setValue(croppedImage, forKey:kCIInputImageKey);
+        transform.setValue(affineTransform, forKey:kCIInputTransformKey);
         return transform.outputImage;
     }
 }
